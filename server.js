1b1b96a36cfbfb32f062630a6c761cf71ba92d7a
andrematt <andrea.num56@gmail.com>
2018-07-06 20:32:50 +0200

    cleaning

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // create a connector to access the database
 let sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('sensorMini.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('data/sensor.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
   }
@@ -8,25 +8,6 @@ let db = new sqlite3.Database('sensorMini.db', sqlite3.OPEN_READWRITE, (err) => 
 console.log(db);
 });
 
-
-/*
-db.serialize(() => {
-  db.each(`SELECT *
-           FROM crossings`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(row.timestamp + "\t" +row.id + "\t" + row.fullname + "\t" + row.x +"\t" + row.y);
-  });
-});
- 
-db.close((err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
-*/
 
 // initialize express
 let express = require('express');
@@ -43,14 +24,14 @@ let selectAllQuery='SELECT * FROM sensor';
 let selectPointNameQuery="SELECT * from sensor where gate = ?"
 
 function findInDb(query, callback){ //una funzione passata come parametro viene eseguita alla fine della funzione che la riceve
-    db.all(selectAll, function(err, rows){ //exec query
+    db.all(query, function(err, rows){ //exec query
 		if(err){ return callback(err) };
 		callback(null, rows);
 	});
 };
 
 function findInDbSingle(query, name, callback){
-	db.all(selectPointNameQuery, name,function(err,rows){
+	db.all(query, name,function(err,rows){
 		if(err){ return callback(err) };
 		callback(null, rows);
 	});
@@ -70,8 +51,8 @@ restapi.get('/sensors', function(req, res){ //entrypoint generico per tutti i da
 restapi.get('/sensor/:gate', function(req, res){
 	let gateName = req.params.gate;
 	if(gateName){
-		findInDbSingle(selectPointNameQuery, gateName, function(err, obj){ //function(err, obj) è cosa fare terminata la query, cioè la callback
-    	if(err){ return next(err) }; //gestisce gli errori (il primo param della callback deve contenere errori)
+		findInDbSingle(selectPointNameQuery, gateName, function(err, obj){ //callback
+    	if(err){ return next(err) }; //gestisce gli errori
 		res.send(obj); //oppure invia la responce
   		});
 	}
